beb75eec739c006717f8cc76c7f7da7857375c41
Winter <jonathan.bold@gmail.com>
2024-06-22 00:54:19 +0100

    changes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #from textnode import TextNode
 import os
 import shutil
+import re
 
 def main():
     __location__ = os.path.realpath(os.path.join(os.getcwd(), 'public'))
@@ -23,6 +24,18 @@ def copy_files(path):
             print('copying file')
             shutil.copyfile(full_path, os.path.join(os.getcwd(), 'public', os.path.relpath(full_path, os.path.join(os.getcwd(), 'static'))))
     return
+
+def extract_title(markdown: str) -> str:
+    if not markdown.lstrip.startswith('# '):
+        raise ValueError("Markdown to start with heading")
+    match = re.match(r'^\s*#\s+(.+)', markdown)
+    if match:
+        return match.group(1).strip()
+    raise ValueError("Markdown doesn't contain a valid title")
+
+def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+    print(f"generating page from {from_path} to {dest_path} using {template_path}")
+    pass
 
 if __name__ == "__main__":
     main()
